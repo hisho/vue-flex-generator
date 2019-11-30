07b90695d8c4e3977e4502dec111eb24ae7991f6
HIsho Ohmasu <hisyou000@gmail.com>
2019-12-01 05:59:49 +0900

    ✨ add core logic

--- src/@js/modules/app.js
+++ src/@js/modules/app.js
@@ -4,13 +4,33 @@ export const app = new Vue({
   el: "#app",
   data: {
     cards: card_data,
-    cardIndex: null,
-    message: "Hello Vue.js"
+    col: `4`,
+    rowGap: `30`,
+    colGap: `20`,
   },
   methods: {
     background: function(index) {
       const imgIndex = String(index + 1).padStart(2, `0`);
-      return { backgroundImage: `url(assets/img/cat${imgIndex}.jpg)` };
+      return {
+        backgroundImage: `url(assets/img/cat${imgIndex}.jpg)`
+      };
+    },
+    rowStyle: function() {
+      return {
+        marginTop: `-${this.rowGap}px`,
+        marginLeft: `-${this.colGap}px`
+      };
+    },
+    colStyle: function() {
+      const widthNuber = ((1 / Number(this.col)) * 100);
+      return {
+        paddingTop: `${this.rowGap}px`,
+        paddingLeft: `${this.colGap}px`,
+        width: `${widthNuber}%`
+      };
+    },
+    StyleWidth: function() {
+      return Math.floor((1 / Number(this.col)) * 100 * 1e4) / 1e4;;
     }
   }
 });
