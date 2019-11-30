b812fd2a8b48339516deff591446f3dab56882bf
HIsho Ohmasu <hisyou000@gmail.com>
2019-12-01 04:31:11 +0900

    🎉 add card section

--- src/@js/modules/app.js
+++ src/@js/modules/app.js
@@ -1,6 +1,16 @@
+import card_data from "../datas/card.json"
+
 export const app = new Vue({
   el: "#app",
   data: {
+    cards: card_data,
+    cardIndex: null,
     message: "Hello Vue.js"
+  },
+  methods: {
+    background: function(index) {
+      const imgIndex = String(index + 1).padStart(2, `0`);
+      return { backgroundImage: `url(assets/img/cat${imgIndex}.jpg)` };
+    }
   }
 });
